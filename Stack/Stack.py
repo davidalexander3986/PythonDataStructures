c9f65f3d0d098dceb18d6dc7f05155f734da6860
David Alexander <d1alexan@ucsd.edu>
2018-08-05 15:49:41 -0700

    Added Stack class, without commenting, so I can focus more on practicing solving problems using a stack

---
+++ Stack/Stack.py
@@ -0,0 +1,43 @@
+
+class Stack:
+    
+    def __init__(self):
+        self.items = []
+        self.count = 0
+        self.stackptr = -1
+
+    def push(self, data):
+        self.items.append(data)
+        self.count += 1
+        self.stackptr += 1
+
+    def pop(self):
+        if self.count > 0:
+            toReturn = self.items[self.stackptr]
+            self.items[self.stackptr] = None
+            self.stackptr -= 1
+            self.count -= 1
+        else:
+            toReturn = None
+        return toReturn
+
+    def peek(self):
+        return self.items[self.stackptr]
+
+    def __str__(self):
+        toReturn  = ""
+        if self.count > 0:
+            strPtr = self.stackptr
+            toReturn += "TOP:\t"
+            while strPtr > -1:
+                toReturn += str(self.items[strPtr]) + "\n\t\t"
+                strPtr -= 1
+        else:
+            toReturn += "STACK IS EMPTY"
+
+        return toReturn
+                
+
+            
+
+        
---
+++ Stack/test.py
@@ -0,0 +1,59 @@
+import stack as st
+
+ST = st.Stack()
+
+def printMenu():
+    print("Commands:")
+    print("Enter p to push\nEnter po to pop\nEnter pe to peek\nEnter d to display contents")
+    print("Enter Q to quit")
+    command = input("Please enter a command: ")
+    
+    return command
+
+def push():
+    number = int(input("Enter a number to push: "))
+    ST.push(number)
+    print(str(number) + " PUSHED to stack")
+    print("Stack is now:")
+    display()
+
+def peek():
+    thing = ST.peek()
+    if thing == None:
+        print("Nothing to PEEK")
+    else:
+        print(str(thing))
+
+def display():
+    print(LL)
+
+def remove():
+    itemPopped = ST.pop()
+    if itemPopped == None:
+        print("Nothing to pop!")
+    else:
+        print(str(itemPopped) + " POPPED from stack")
+    print("Stack is now: ")
+    print(LL)
+
+def main():
+    
+    print("Now running interface for Stack...")
+    command = ""
+    while True:
+        command = printMenu()
+        if command == "Q":
+            break
+        elif command == "p":
+            push()
+        elif command == "po":
+            pop()
+        elif command == "pe":
+            peek()
+        elif command == "d":
+            display()
+        else:
+            print("Incorrect command!")
+
+        print()
+main()
